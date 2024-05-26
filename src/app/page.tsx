1e45ea913149f6eaecf9628efa62ccf84cb0ba1b
tom-takeru <tom.2461.ty@gmail.com>
2024-05-26 21:48:34 +0900

    Fix folder position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import GroupDescription from "./components/top/GroupDescription";
+import GroupDescription from "../components/top/GroupDescription";
 import Layout from "./layout";
-import MemberList from "./components/top/MemberList";
-import PortfolioList from "./components/top/PortfolioList";
-import SnsList from "./components/top/SnsList";
-import Header from "./components/common/Header";
-import Footer from "./components/common/Footer";
+import MemberList from "../components/top/MemberList";
+import PortfolioList from "../components/top/PortfolioList";
+import SnsList from "../components/top/SnsList";
+import Header from "../components/common/Header";
+import Footer from "../components/common/Footer";
 
 export default function Home() {
   return (
