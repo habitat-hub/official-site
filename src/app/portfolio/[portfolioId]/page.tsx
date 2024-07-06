9efece232c143cd651541450ca2279fd8c63c269
Mellbrother <keisuke0907@outlook.jp>
2024-07-06 19:12:27 +0900

    Refactor portfolio page component

--- src/app/portfolio/[portfolioId]/page.tsx
+++ src/app/portfolio/[portfolioId]/page.tsx
@@ -15,21 +15,13 @@ export default function Portfolio({
 
   return (
     <PortfolioTemplate
-      mainVisualUrl="/portfolio/daydule/daydule_home.png"
-      title="Daydule"
-      releaseDate="2023/09"
-      siteUrl="https://daydule.habitat-hub.com/main"
-      enabledDevice={["PC"]}
-      siteImageUrls={[
-        "/portfolio/daydule/daydule_site_1.png",
-        "/portfolio/daydule/daydule_site_2.png",
-        "/portfolio/daydule/daydule_site_3.png",
-        "/portfolio/daydule/daydule_site_4.png",
-      ]}
-      description={`Dayduleは、TODOや予定をもとに自動でスケジュールを作成してくれるサイトです。<br/>
-      必要な作業はTODOと予定の登録、そしてスケジュールボタンを押すだけ！<br/>
-      自分でスケジュール考えることが苦手な人や、時間がない人におすすめです。
-      `}
+      mainVisualUrl={portfolioContent.mainVisualUrl}
+      title={portfolioContent.title}
+      releaseDate={portfolioContent.releaseDate}
+      siteUrl={portfolioContent.siteUrl}
+      enabledDevice={portfolioContent.enabledDevice}
+      siteImageUrls={portfolioContent.siteImageUrls}
+      description={portfolioContent.description}
     />
   );
 }
